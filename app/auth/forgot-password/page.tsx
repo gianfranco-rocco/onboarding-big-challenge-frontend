a43e809e1fedf23bc44916af9e7ca6ca204e8306
Gianfranco Rocco <gianfranco.rocco@lightit.io>
2022-11-11 16:21:43 -0300

    feat: forgot password layout

--- app/auth/forgot-password/page.tsx
+++ app/auth/forgot-password/page.tsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import { ButtonPrimary, Link } from "../../../components/ui/buttons"
+import { Form } from "../../../components/ui/forms/Form"
+import { Input } from "../../../components/ui/inputs"
 
 const ForgotPasswordPage = () => {
-  return (
-    <div>ForgotPasswordPage</div>
-  )
-}
+    return (
+        <Form title="Restore your password">
+            <div>
+                <Input
+                    label="Email address"
+                    name="email"
+                    type="email"
+                    autoComplete="email"
+                    placeholder="johndoe@email.com"
+                    required
+                />
+            </div>
 
-export default ForgotPasswordPage
+            <ButtonPrimary type="submit">
+                Send password recovery email
+            </ButtonPrimary>
+
+            <div className="flex justify-center">
+                <Link href="/auth/login">
+                  Log in
+                </Link>
+            </div>
+        </Form>
+    )
+  }
+  
+  export default ForgotPasswordPage
