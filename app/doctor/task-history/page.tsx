43ac723a243e37ddaa0fa00abea4b90155631f78
Gianfranco Rocco <gianfranco.rocco@lightit.io>
2022-11-15 15:14:34 -0300

    feat: task history page

--- app/doctor/task-history/page.tsx
+++ app/doctor/task-history/page.tsx
@@ -1,9 +1,75 @@
+'use client'
+
 import React from 'react'
+import { Badge } from '../../../components/ui/badges'
+import { Link } from '../../../components/ui/buttons'
+import { IColumn, IPagination, IRow, Table } from '../../../components/ui/tables'
+import { useState, useEffect } from 'react';
+import { useRouter, useSearchParams } from 'next/navigation'
+import { badge } from '../../../utils'
+
+const columns: IColumn[] = [
+  { field: 'submissionTitle', name: 'submission title', className: 'font-bold' },
+  { field: 'patientName', name: 'patient name', className: 'font-bold' }, 
+  { field: 'createdAt', name: 'created at' }, 
+  { 
+    field: 'status', 
+    name: 'status',
+    renderCell: (row: IRow) => <Badge type={badge.typeBasedOnStatus(row.status)} className='capitalize'>{row.status.replace('_', ' ')}</Badge>
+  },
+  { 
+    field: 'view', 
+    renderCell: (row: IRow) => <Link href={`/doctor/submissions/${row.id}`}>View more</Link>
+  }
+]
 
 const TaskHistoryPage = () => {
-  return (
-    <div>TaskHistoryPage</div>
-  )
+  const { get: params } = useSearchParams()
+  const router = useRouter()
+
+  const [rows, setRows] = useState<IRow[]>([
+    {
+      id: 1,
+      submissionTitle: 'Submission 1',
+      patientName: 'Gianfranco Rocco',
+      createdAt: '2022-10-10',
+      status: 'in_progress',
+    },
+    {
+      id: 2,
+      submissionTitle: 'Submission 2',
+      patientName: 'Gianfranco Rocco',
+      createdAt: '2022-10-12',
+      status: 'done',
+    },
+  ])
+
+  const currentPage = Number(params('page') || 1)
+
+  const [pagination, setPagination] = useState<IPagination>({
+    count: 2,
+    total: 330,
+    perPage: 15,
+    currentPage: 1,
+    totalPages: 8,
+    links: {
+      previous: 'asd',
+      next: 'asd'
+    }
+  })
+
+  useEffect(() => {
+    setPagination(curr => ({
+      ...curr,
+      currentPage
+    }))
+  }, [currentPage])
+  
+  const handlePagination = (page: number) => {
+    router.push(`/doctor/task-history?page=${page}`)
+  }
+
+  return <Table columns={columns} rows={rows} pagination={pagination} handlePagination={handlePagination} />
 }
 
 export default TaskHistoryPage
