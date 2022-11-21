49f0a0a6a1d1c6217abb5400674fc7559006a6f9
Gianfranco Rocco <gianfranco.rocco@lightit.io>
2022-11-21 17:43:32 -0300

    feat: link button optional target

--- components/ui/buttons/Link.tsx
+++ components/ui/buttons/Link.tsx
@@ -3,12 +3,12 @@ import NextLink from "next/link"
 
 interface Props {
     href: string;
-    target: React.HTMLAttributeAnchorTarget;
+    target?: React.HTMLAttributeAnchorTarget;
     download?: boolean;
     children: React.ReactNode;
 }
 
-export const Link: FC<Props> = ({ href, target, download, children }) => {
+export const Link: FC<Props> = ({ href, target = '_self', download, children }) => {
     return (
         <NextLink href={href} target={target} download={download} className="text-sm font-medium text-indigo-600 hover:text-indigo-500">
             {children}
