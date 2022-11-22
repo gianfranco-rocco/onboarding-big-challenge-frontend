9e135a646d013cd41f145a937f50939c522d7feb
Gianfranco Rocco <gianfranco.rocco@lightit.io>
2022-11-22 16:24:50 -0300

    feat: patient root layout

---
+++ app/patient/layout.tsx
@@ -0,0 +1,18 @@
+'use client'
+
+import { HomeIcon, PlusCircleIcon } from "@heroicons/react/24/outline"
+import { MainLayout } from "../../components/layouts"
+import { INavigation } from "../../components/ui"
+
+const navigation: INavigation[] = [
+  { name: 'Home', href: '/patient/submissions', icon: HomeIcon },
+  { name: 'New submission', href: '/patient/submissions/create', icon: PlusCircleIcon },
+]
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return <MainLayout navigation={navigation}>{children}</MainLayout>
+}
